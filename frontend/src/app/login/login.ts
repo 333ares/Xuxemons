89c7baebe7117e836b50c0a11eedb730de1026db
OBB-06 <olayabaabidane@gmail.com>
2026-02-18 19:25:49 +0100

    Comienzo el formulario  del login con el campo ID  en el html. Comienzo con los validadores y los metodos en el ts.

--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
 export class Login {
+  loginForm: FormGroup;
+  errorMessage: string = '';
 
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.loginForm = this.fb.group({
+      userId: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) return;
+
+    const { userId, password } = this.loginForm.value;
+  }
 }
