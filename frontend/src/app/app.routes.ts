8d04f9bee42f80ab23ecb3fe7e43caf18b75a791
amiyasarkaz <deividsonic2@gmail.com>
2026-02-18 18:50:42 +0100

    Realizacion de codigo en los archivos TS, HTML y CSS
    He agregado todo el código HTML con las secciones de recompensas, la colección, la actividad, las estadísticas y el xuxemon.
    
    He realizado los estilos para que se parezcan al diseño original: utilizo una cuadrícula en la versión de escritorio, tarjetas, sombras, colores pastel y todo está bien alineado.
    
    He preparado el TypeScript con todos los datos necesarios, como las recompensas, la colección, las solicitudes, las estadísticas y el xuxemon, y el componente funciona de manera independiente.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PaginaPrincipal } from './pagina-principal/pagina-principal';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PaginaPrincipal
+  }
+];
