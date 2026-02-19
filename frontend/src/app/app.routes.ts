08ac65569c3303b6eacbe05d1fda8022c0600497
amiyasarkaz <deividsonic2@gmail.com>
2026-02-19 19:23:02 +0100

    Archivos TS, HTML y CSS empezados
    He empezado a hacer el codigo de los tres archivos para crear la pantalla de usuario. Aun no esta acabado, cuando este listo hare un commit explicando brevemente los archivos.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { PaginaPrincipal } from './pagina-principal/pagina-principal';
+import { PerfilComponent } from './perfil/perfil';
+
 
 export const routes: Routes = [
   {
     path: '',
     component: PaginaPrincipal
+  },
+  {
+    path: 'perfil',
+    component: PerfilComponent
   }
 ];
+
--- frontend/src/app/perfil/perfil.ts
+++ frontend/src/app/perfil/perfil.ts
@@ -1,11 +1,57 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-perfil',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './perfil.html',
-  styleUrl: './perfil.css',
+  styleUrls: ['./perfil.css']
 })
-export class Perfil {
+export class PerfilComponent {
 
+  // Datos del usuario (luego vendrán del backend)
+  usuario = {
+    avatar: 'assets/avatar.png',
+    username: 'AetherByte',
+    userId: '#AetherByte4821',
+    rol: 'Jugador/a',
+    email: 'aetherbyte@gmail.com',
+    telefono: '+34 612 843 957',
+    fechaRegistro: '14 Feb 2026',
+    nombre: 'Aether',
+    apellidos: 'Byte'
+  };
+
+  // Estadísticas del usuario
+  stats = {
+    ganadas: 18,
+    jugadas: 27,
+    racha: 5
+  };
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      nombre: [this.usuario.nombre],
+      apellidos: [this.usuario.apellidos],
+      email: [this.usuario.email],
+      password: ['']
+    });
+  }
+
+  guardarCambios() {
+    console.log('Datos enviados:', this.form.value);
+  }
+
+  descartarCambios() {
+    this.form.patchValue({
+      nombre: this.usuario.nombre,
+      apellidos: this.usuario.apellidos,
+      email: this.usuario.email,
+      password: ''
+    });
+  }
 }
