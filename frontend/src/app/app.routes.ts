675ca609103a331aa53c9b4fe280b10f409a9ea7
OBB-06 <olayabaabidane@gmail.com>
2026-02-19 18:47:36 +0100

    Rutas
    + pequeños cambios en 3 clases mas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Registro } from './registro/registro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Reedireccion automatica a Login
+  { path: '',
+    redirectTo: 'login',
+    pathMatch: 'full' //Solo aplica si es / (Eso significa que si el / tiene algo mas detras no reedirigiria a Login)
+  },
+
+  // Ruta Login
+  { path: 'login',
+    component: Login
+  },
+
+  // Ruta Registro
+  { path: 'registro',
+    component: Registro
+  },
+
+  // Si alguien entra a una pagina inexistente lo reedirige al login.
+  { path: '**',
+    redirectTo: 'login'
+  }
+];
