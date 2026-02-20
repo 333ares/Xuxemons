87bdeb4fbd843fbcee40bae7a3d930fc376f8107
OBB-06 <olayabaabidane@gmail.com>
2026-02-20 18:51:27 +0100

    Creo el componente de recuperar
    Este componente lo creo para el apartado de recuperar tu contraseña que sale en el login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,18 @@ import { Routes } from '@angular/router';
 import { PaginaPrincipal } from './pagina-principal/pagina-principal';
 import { Login } from './login/login';
 import { Registro } from './registro/registro';
+import { Recuperar } from './recuperar/recuperar';
 
 export const routes: Routes = [
   // Reedireccion automatica a Login
   { path: '',
     redirectTo: 'login',
     pathMatch: 'full' //Solo aplica si es / (Eso significa que si el / tiene algo mas detras no reedirigiria a Login)
+  },
+
+  // Si alguien entra a una pagina inexistente lo reedirige al login.
+  { path: '**',
+    redirectTo: 'login'
   },
 
   // Ruta Login
@@ -20,10 +26,12 @@ export const routes: Routes = [
     component: Registro
   },
 
-  // Si alguien entra a una pagina inexistente lo reedirige al login.
-  { path: '**',
-    redirectTo: 'login'
+  // Ruta Recuperar
+  { path: 'recuperar',
+    component: Recuperar
   },
+
+  // Ruta Pagina Principal
   {
     path: '',
     component: PaginaPrincipal
---
+++ frontend/src/app/recuperar/recuperar.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, AbstractControl, ValidationErrors } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-recuperar',
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './recuperar.html',
+  styleUrl: './recuperar.css',
+})
+
+export class Recuperar {
+
+}
