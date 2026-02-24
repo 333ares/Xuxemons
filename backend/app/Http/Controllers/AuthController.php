4c78bd292c71bd313f9c9076112b9c8246991d6a
333ares <aresgp5335@gmail.com>
2026-02-24 17:25:22 +0100

    Modificación en el yml + rutas + errores controlador
    He modificado el yml, porque al recrear el contenedor de Laravel, Apache empezó a devolver un error 500, se debía al sed, asi que, lo he modificado para que Apache apunte correctamente a /public y evite el error 500
    He añadido la ruta de ver la información de usuario y he modificado  los nombres de las rutas login y regitrso.
    En el controlador Auth, he modificado algunos errores, por ejemplo, darle un valor temporal a public_id, par que no de error y pasarle parametros a "unique".

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -16,25 +16,24 @@ class AuthController extends Controller
     {
         // Validamos que los datos sean correctos
         $validator = Validator::make($request->all(), [
+            'public_id' => 'nullable',
             'name' => 'required|string',
             'surname' => 'required|string',
-            'email' => 'required|unique',
+            'email' => 'required|unique:users,email',
             'password' => 'required'
         ]);
 
         // Si falla mostramos error
         if ($validator->fails()) {
-            return response(
-                [
-                    'message' => 'error',
-                    'errors' => $validator->errors()
-                ],
-                400
-            );
+            return response()->json([
+                'message' => 'error',
+                'errors' => $validator->errors()
+            ], 400);
         }
 
         // Creamos usuario
         $usuario = User::create([
+            'public_id' => '#NombreXXXX',
             'name' => $request->name,
             'surname' => $request->surname,
             'email' => $request->email,
@@ -98,7 +97,7 @@ class AuthController extends Controller
         ], 200);
     }
 
-    public function logoutUsuario(Request $request)
+    public function logoutUsuario()
     {
         try {
             // Invalidamos el token JWT
