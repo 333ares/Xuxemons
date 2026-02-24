1a0f730832d4830673317079f2dc086e781410c0
333ares <aresgp5335@gmail.com>
2026-02-24 16:53:17 +0100

    Cambios para jwt
    En el modelo he implementado JWTSubject. Esto le dice a JWT cómo identificar al usuario dentro del token.
    Ademas en el auth.php modifico el guardo por defecto para que laravel use JWT en lugar de Sanctum.
    En las rutas protegidas (como: logout) implemento middleware "auth:api" en lugar de "auth:santum". En las rutas publicas no hace falta (como: login o registro)

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
@@ -27,4 +28,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
     ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
