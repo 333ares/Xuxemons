a6cf1b2d5c04fa5d9893b3eacafce63264b73188
OBB-06 <olayabaabidane@gmail.com>
2026-02-26 16:54:14 +0100

    Elimino el anterior y creo un nuevo componente

--- frontend/src/app/perfil-usuario/perfil-usuario.spec.ts
+++ frontend/src/app/perfil-usuario/perfil-usuario.spec.ts
@@ -1,17 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Perfil } from './perfil';
 
-describe('Perfil', () => {
-  let component: Perfil;
-  let fixture: ComponentFixture<Perfil>;
+import { PerfilUsuario } from './perfil-usuario';
+
+describe('PerfilUsuario', () => {
+  let component: PerfilUsuario;
+  let fixture: ComponentFixture<PerfilUsuario>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Perfil]
+      imports: [PerfilUsuario]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Perfil);
+    fixture = TestBed.createComponent(PerfilUsuario);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
