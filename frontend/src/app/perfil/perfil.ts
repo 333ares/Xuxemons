77510645d7435cb1322bed33048f1e970eb3688e
OBB-06 <olayabaabidane@gmail.com>
2026-02-26 16:45:37 +0100

    Empezando de nuevo el Perfil de usuario

--- frontend/src/app/perfil/perfil.ts
+++ frontend/src/app/perfil/perfil.ts
@@ -9,49 +9,6 @@ import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
   templateUrl: './perfil.html',
   styleUrls: ['./perfil.css']
 })
-export class PerfilComponent {
+export class Perfil {
 
-  // Datos del usuario (luego vendrán del backend)
-  usuario = {
-    avatar: 'assets/avatar.png',
-    username: 'AetherByte',
-    userId: '#AetherByte4821',
-    rol: 'Jugador/a',
-    email: 'aetherbyte@gmail.com',
-    telefono: '+34 612 843 957',
-    fechaRegistro: '14 Feb 2026',
-    nombre: 'Aether',
-    apellidos: 'Byte'
-  };
-
-  // Estadísticas del usuario
-  stats = {
-    ganadas: 18,
-    jugadas: 27,
-    racha: 5
-  };
-
-  form: FormGroup;
-
-  constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
-      nombre: [this.usuario.nombre],
-      apellidos: [this.usuario.apellidos],
-      email: [this.usuario.email],
-      password: ['']
-    });
-  }
-
-  guardarCambios() {
-    console.log('Datos enviados:', this.form.value);
-  }
-
-  descartarCambios() {
-    this.form.patchValue({
-      nombre: this.usuario.nombre,
-      apellidos: this.usuario.apellidos,
-      email: this.usuario.email,
-      password: ''
-    });
-  }
 }
