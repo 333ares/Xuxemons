2d0dbb162226a7f7561d0e71b2c282831ca3c535
OBB-06 <olayabaabidane@gmail.com>
2026-03-02 17:04:45 +0100

    Creación de todas las pantallas
    creación de todas las pantallas que faltan, para que se pueda navegar por el menu inferior

---
+++ frontend/src/app/amigos/amigos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-amigos',
+  imports: [],
+  templateUrl: './amigos.html',
+  styleUrl: './amigos.css',
+})
+export class Amigos {
+
+}
---
+++ frontend/src/app/batalla/batalla.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-batalla',
+  imports: [],
+  templateUrl: './batalla.html',
+  styleUrl: './batalla.css',
+})
+export class Batalla {
+
+}
