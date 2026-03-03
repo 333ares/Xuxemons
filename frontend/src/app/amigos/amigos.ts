967d68898dcc6a2a7c9f2252a934452a904855ce
OBB-06 <olayabaabidane@gmail.com>
2026-03-03 19:22:48 +0100

    amigos mantenimiento

--- frontend/src/app/amigos/amigos.ts
+++ frontend/src/app/amigos/amigos.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-amigos',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './amigos.html',
-  styleUrl: './amigos.css',
+  styleUrls: ['./amigos.css'],
 })
-export class Amigos {
-
-}
+export class Amigos {}
