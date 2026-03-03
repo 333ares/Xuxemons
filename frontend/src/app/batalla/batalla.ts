bd232bf0ff24f98709681a9e079c0599f01eda6e
OBB-06 <olayabaabidane@gmail.com>
2026-03-03 19:22:36 +0100

    batalla mantenimeinto

--- frontend/src/app/batalla/batalla.ts
+++ frontend/src/app/batalla/batalla.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-batalla',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './batalla.html',
   styleUrl: './batalla.css',
 })
-export class Batalla {
-
-}
+export class Batalla {}
