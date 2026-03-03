499e0e5481a06e676b051faaadf810a250f06ddc
OBB-06 <olayabaabidane@gmail.com>
2026-03-03 17:12:06 +0100

    Rutas para poder navegar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,6 +5,11 @@ import { Registro } from './registro/registro';
 import { Recuperar } from './recuperar/recuperar';
 import { PerfilUsuario } from './perfil-usuario/perfil-usuario';
 import { AuthGuard } from './guards/auth-guard';
+import { Amigos } from './amigos/amigos';
+import { Batalla } from './batalla/batalla';
+import { Chat } from './chat/chat';
+import { Mochila } from './mochila/mochila';
+import { Xuxedex } from './xuxedex/xuxedex';
 
 export const routes: Routes = [
   // Reedireccion automatica a Login
@@ -27,13 +32,49 @@ export const routes: Routes = [
   {
     path: 'paginaPrincipal',
     component: PaginaPrincipal,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
   },
+
   // Ruta Perfil del Usuario
   {
     path: 'perfilUsuario',
     component: PerfilUsuario,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
+  },
+
+  // Ruta Amigos
+  {
+    path: 'amigos',
+    component: Amigos,
+    canActivate: [AuthGuard],
+  },
+
+  // Ruta Batalla
+  {
+    path: 'batalla',
+    component: Batalla,
+    canActivate: [AuthGuard],
+  },
+
+  // Ruta Chat
+  {
+    path: 'chat',
+    component: Chat,
+    canActivate: [AuthGuard],
+  },
+
+  // Ruta Mochila
+  {
+    path: 'mochila',
+    component: Mochila,
+    canActivate: [AuthGuard],
+  },
+
+  // Ruta Xuxedex
+  {
+    path: 'xuxedex',
+    component: Xuxedex,
+    canActivate: [AuthGuard],
   },
 
   // Si alguien entra a una pagina inexistente lo reedirige al login. (Esto se tiene que dejar al final de las rutas)
