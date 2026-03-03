6208f5e4209a141fe9b7b36f922a4e4ba17c2d50
OBB-06 <olayabaabidane@gmail.com>
2026-03-03 19:22:24 +0100

    chat mantenimiento

--- frontend/src/app/chat/chat.ts
+++ frontend/src/app/chat/chat.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-chat',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './chat.html',
   styleUrl: './chat.css',
 })
-export class Chat {
-
-}
+export class Chat {}
