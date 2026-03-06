cbd091010b2d9dd5a7045b26f5ad247760f960d5
333ares <aresgp5335@gmail.com>
2026-03-06 20:25:13 +0100

    Funciones de añadir xuxemon y ver lista de xuxemons añadidas
    Junto con las rutas api.
    para añadir un xuxemon he creado un controlador de admin

--- backend/app/Http/Controllers/XuxemonsController.php
+++ backend/app/Http/Controllers/XuxemonsController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Xuxemons;
 use Illuminate\Http\Request;
 
 class XuxemonsController extends Controller
 {
-    //
+    public function listarXuxemons(Request $request)
+    {
+        $xuxemons = Xuxemons::where('user_id', $request->user()->id)->get();
+
+        if (count($xuxemons) <= 0) {
+            return response()->json([
+                'message' => 'error',
+                'errors' => 'No tienes xuxemons aún'
+            ], 400);
+
+        } else {
+            return response()->json([
+                'message' => 'success',
+                'xuxemons' => $xuxemons
+            ], 201);
+        }
+    }
 }
