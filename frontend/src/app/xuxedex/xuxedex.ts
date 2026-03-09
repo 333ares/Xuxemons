8e38b31dc420904a0bfeefa97105be3ae43c0cc7
OBB-06 <olayabaabidane@gmail.com>
2026-03-09 19:29:57 +0100

    Empezando el ts
    crear componente Xuxedex y conexión a la API, añadir vista principal para listar y agrupar xuxemons

--- frontend/src/app/xuxedex/xuxedex.ts
+++ frontend/src/app/xuxedex/xuxedex.ts
@@ -5,6 +5,8 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { ICONS } from '../shared/icons';
 import { Nav } from '../shared/nav/nav';
+
+// Modelo individual de cada criatura.
 export interface Xuxemon {
   id: number;
   name: string;
@@ -16,6 +18,7 @@ export interface Xuxemon {
   updated_at?: string;
 }
 
+//Sirve para agrupar en la interfaz gráfica a todos los Xuxemons del mismo nombre.
 export interface XuxemonGrupo {
   nombre: string;
   type: 'agua' | 'tierra' | 'aire';
@@ -31,12 +34,49 @@ export interface XuxemonGrupo {
   templateUrl: './xuxedex.html',
   styleUrl: './xuxedex.css',
 })
+
 export class Xuxedex {
   icons: Record<string, SafeHtml> = {};
 
-  constructor(private sanitizer: DomSanitizer) {
+  cargando = true;
+  error = '';
+
+  todosLosGrupos: XuxemonGrupo[] = [];
+  grupoSeleccionado: XuxemonGrupo | null = null;
+
+  private readonly API_URL = 'http://localhost:8000/api';
+
+  constructor(
+    private sanitizer: DomSanitizer, // Angular bloquea por defecto el código HTML/SVG insertado dinámicamente para evitar ataques XSS
+    private http: HttpClient, // Para hacer peticiones web
+  ) {
     Object.keys(ICONS).forEach((key) => {
       this.icons[key] = this.sanitizer.bypassSecurityTrustHtml(ICONS[key]);
     });
   }
+
+  // Controla el estado de la interfaz usando las variables cargando y error
+  cargarXuxemons(): void {
+    this.cargando = true;
+    this.error = '';
+    this.http.get<{ message: string; xuxemons: Xuxemon[] }>(`${this.API_URL}/xuxemons`).subscribe({// Hace una petición GET a la API
+      next: (res) => {// Agrupa la lista de Xuxemons recibidos
+        this.todosLosGrupos = this.agruparPorNombre(res.xuxemons);
+        if (this.todosLosGrupos.length > 0) {
+          this.grupoSeleccionado = this.todosLosGrupos[0];
+        }
+        this.cargando = false;
+      },
+      error: (err) => { // Si el servidor devuelve un error 400 (Bad Request), simplemente vacía la lista.
+        if (err.status === 400) {
+          this.todosLosGrupos = [];
+        } else {
+          this.error = "No s'han pogut carregar els Xuxemons.";
+        }
+        this.cargando = false;
+      },
+    });
+  }
+
+  private agruparPorNombre(lista: Xuxemon[]): XuxemonGrupo[] {}
 }
