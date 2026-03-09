a7f56dc8b587888bd518f31bb83e2b427d376840
333ares <aresgp5335@gmail.com>
2026-03-09 19:16:12 +0100

    Listar usuarios implementado
    He comentado la lógica de la función "agregarXuxemon", porque me he vuelto a leer el enunciado y pide que la asignación sean aleatoria y yo la hice manual.
    Además he creado una función con su ruta api para listar a todos los usuarios.

--- backend/app/Http/Controllers/AdminController.php
+++ backend/app/Http/Controllers/AdminController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers;
 use App\Models\Xuxemons;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 
 class AdminController extends Controller
 {
     public function agregarXuxemon(Request $request)
     {
+        /*
         $validator = Validator::make($request->all(), [
             'name' => 'required|string',
             'type' => 'required|in:aire,tierra,agua',
@@ -40,6 +42,33 @@ class AdminController extends Controller
                 'message' => 'error',
                 'errors' => $validator->errors()
             ], 400);
+        }*/
+    }
+
+    public function listarUsuarios(Request $request)
+    {
+        $admin = $request->user()->id;
+
+        if ($admin === 1) {
+            $usuarios = User::all();
+
+            if (!$usuarios) {
+                return response()->json([
+                    'message' => 'error',
+                    'errors' => 'No se han podido listar los usuarios'
+                ], 404);
+            } else {
+                return response()->json([
+                    'message' => 'success',
+                    'xuxemons' => $usuarios
+                ], 201);
+            }
+            
+        } else {
+             return response()->json([
+                    'message' => 'error',
+                    'errors' => 'No tienes suficientes permisos para ejecutar esta función'
+                ], 400);
         }
     }
 }
