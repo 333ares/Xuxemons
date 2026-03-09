68897951ccdfcc93c420270b074fd2d40a19e0f7
OBB-06 <olayabaabidane@gmail.com>
2026-03-09 17:29:31 +0100

    Optimización del menu inferior

--- frontend/src/app/xuxedex/xuxedex.ts
+++ frontend/src/app/xuxedex/xuxedex.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { ICONS } from '../shared/icons';
 
 @Component({
   selector: 'app-xuxedex',
@@ -8,4 +10,12 @@ import { RouterLink } from '@angular/router';
   templateUrl: './xuxedex.html',
   styleUrl: './xuxedex.css',
 })
-export class Xuxedex {}
+export class Xuxedex {
+  icons: Record<string, SafeHtml> = {};
+
+  constructor(private sanitizer: DomSanitizer) {
+    Object.keys(ICONS).forEach((key) => {
+      this.icons[key] = this.sanitizer.bypassSecurityTrustHtml(ICONS[key]);
+    });
+  }
+}
