32c93765c46ac23c704ba9235d5a3038ba7e18e1
333ares <aresgp5335@gmail.com>
2026-03-10 17:43:12 +0100

    Función agregarXuxemon
    Ahora cumple con lo especificado, se genera un xuxemon random y se asigna a un usuario.

--- backend/app/Http/Controllers/AdminController.php
+++ backend/app/Http/Controllers/AdminController.php
@@ -4,45 +4,51 @@ namespace App\Http\Controllers;
 
 use App\Models\Xuxemons;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 use App\Models\User;
+use App\Models\XuxemonsInfo;
 
 class AdminController extends Controller
 {
     public function agregarXuxemon(Request $request)
     {
-        /*
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string',
-            'type' => 'required|in:aire,tierra,agua',
-            'user_id' => 'required|integer'
-        ]);
+        // Cogemos el id del usuario que hace la petición
+        $admin = $request->user()->id;
 
-        if ($validator->fails()) {
-            return response()->json([
-                'message' => 'error',
-                'errors' => $validator->errors()
-            ], 400);
+        // Si el id es 1, es el admin
+        if ($admin === 1) {
+            // Randomizamos un número del 1 al 48 (total de xuxemons)
+            $xuxemon_id = random_int(1, 48);
+
+            // Buscamos el id randomizado
+            $xuxemon = XuxemonsInfo::where('id', $xuxemon_id)->first();
+
+            // Si no se encuentra, mostramos error
+            if (!$xuxemon) {
+                return response()->json([
+                    'message' => 'error',
+                    'errors' => 'No se ha encontrado un xuxemon con ese id'
+                ], 400);
+            }
+
+            // Añadimos el xuxemon 
+            $xuxemonUsuario = Xuxemons::create([
+                'name' => $xuxemon->name,
+                'type' => $xuxemon->type,
+                'user_id' => $request->user_id,
+            ]);
+
+            if ($xuxemonUsuario) {
+                return response()->json([
+                    'message' => 'Xuxemon añadido correctamente',
+                    'xuxemon' => $xuxemon
+                ], 201);
+            } else {
+                return response()->json([
+                    'message' => 'error',
+                    'errors' => 'No se ha podido añadir el xuxemon'
+                ], 400);
+            }
         }
-
-        $xuxemon = Xuxemons::create([
-            'name' => $request->name,
-            'type' => $request->type,
-            'user_id' => $request->user_id,
-        ]);
-
-        if ($xuxemon) {
-            return response()->json([
-                'message' => 'Xuxemon añadido correctamente'
-            ], 201);
-
-            // Si no, mostramos el error
-        } else {
-            return response()->json([
-                'message' => 'error',
-                'errors' => $validator->errors()
-            ], 400);
-        }*/
     }
 
     public function listarUsuarios(Request $request)
