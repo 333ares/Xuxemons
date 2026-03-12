9cbd8f407a3098102aba934e0a2fa45c48a5cc55
OBB-06 <olayabaabidane@gmail.com>
2026-03-12 17:41:10 +0100

    Creación de componentes
    Como queremos tenerlo todo ordenado y facil de encontrar vamos ha crear varios componentes para dividirlo mejor todo.
    
    - Gestion de usuarios: Para que el admin adminisre a los usuarios
    
    - Agregar Xuxemons/Objetos: Para que el administrador pueda agregarles a los usuarios xuxemons y objetos

---
+++ frontend/src/app/agregar-objeto/agregar-objeto.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agregar-objeto',
+  imports: [],
+  templateUrl: './agregar-objeto.html',
+  styleUrl: './agregar-objeto.css',
+})
+export class AgregarObjeto {
+
+}
