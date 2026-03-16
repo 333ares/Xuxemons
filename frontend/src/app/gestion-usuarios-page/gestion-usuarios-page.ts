b5673b9da9d82b225ccb827e1a3005b7dbf200a1
OBB-06 <olayabaabidane@gmail.com>
2026-03-16 19:30:39 +0100

    Creacion del componente gestion-usuarios-page
    gestion-usuarios es un componente puro que solo sabe renderizar su card. Entonces por eso creo este componente para que tenga exactamente el mismo header y el mismo max-width: 1400px centrado.

---
+++ frontend/src/app/gestion-usuarios-page/gestion-usuarios-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-usuarios-page',
+  imports: [],
+  templateUrl: './gestion-usuarios-page.html',
+  styleUrl: './gestion-usuarios-page.css',
+})
+export class GestionUsuariosPage {
+
+}
