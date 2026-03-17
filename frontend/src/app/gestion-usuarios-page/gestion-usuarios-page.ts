29a91207b5a046a558a62ecdbbffa7c901f3806c
OBB-06 <olayabaabidane@gmail.com>
2026-03-17 16:44:36 +0100

    La gestion de usuarios para que este centrada
    - La gestión de usuarios estaba sin cabecera y no estaba centrada así que con este componente básicamente se centra a la pantalla para coger el formato de básicamente todas las demas paginas

--- frontend/src/app/gestion-usuarios-page/gestion-usuarios-page.ts
+++ frontend/src/app/gestion-usuarios-page/gestion-usuarios-page.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { Nav } from '../shared/nav/nav';
+import { GestionUsuarios } from '../gestion-usuarios/gestion-usuarios';
 
 @Component({
   selector: 'app-gestion-usuarios-page',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink, Nav, GestionUsuarios],
   templateUrl: './gestion-usuarios-page.html',
-  styleUrl: './gestion-usuarios-page.css',
+  styleUrls: ['./gestion-usuarios-page.css'],
 })
-export class GestionUsuariosPage {
-
-}
+export class GestionUsuariosPage {}
