b5724e99d7c2b6fabc21a260b4675d09e9c618bb
333ares <aresgp5335@gmail.com>
2026-03-19 17:16:46 +0100

    Logica agregar xuxemon

--- frontend/src/app/services/auth.ts
+++ frontend/src/app/services/auth.ts
@@ -128,4 +128,8 @@ export class Auth {
   listarUsuarios(): Observable<any> {
     return this.http.get(`${this.apiUrl}/listarUsuarios`, { headers: this.getHeaders() });
   }
+
+  agregarXuxemon(userId: number): Observable<any> {
+    return this.http.post(`${this.apiUrl}/agregarXuxemon`, { user_id: userId }, { headers: this.getHeaders() });
+  }
 }
