70cb01f62c1d3484452e1699e459fe55ccddd6d8
OBB-06 <olayabaabidane@gmail.com>
2026-03-26 19:00:37 +0100

    Pongo las funcionalidades del admin en parametros
    + corecciones en el css

--- frontend/src/app/parametros/parametros.ts
+++ frontend/src/app/parametros/parametros.ts
@@ -2,12 +2,23 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { Adminav } from '../shared/adminav/adminav';
-import { GestionUsuarios } from '../gestion-usuarios/gestion-usuarios';
+
+// Aquí importamos tus 3 componentes de configuración
+import { GeneracionDiaria } from '../generacion-diaria/generacion-diaria';
+import { ProbabilidadInfeccion } from '../probabilidad-infeccion/probabilidad-infeccion';
+import { CrecimientoXuxemons } from '../crecimiento-xuxemons/crecimiento-xuxemons'; // <- Ajusta la ruta si es necesario
 
 @Component({
   selector: 'app-parametros',
   standalone: true,
-  imports: [CommonModule, Adminav, RouterLink],
+  imports: [
+    CommonModule,
+    Adminav,
+    RouterLink,
+    GeneracionDiaria,
+    ProbabilidadInfeccion,
+    CrecimientoXuxemons,
+  ],
   templateUrl: './parametros.html',
   styleUrl: './parametros.css',
 })
