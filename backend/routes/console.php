8690e72f49effe58ef0c0aa51c43ab84bf639195
333ares <aresgp5335@gmail.com>
2026-03-26 17:22:35 +0100

    Añadir el job al schedule
    Primero miro la configuración del admin.
    Despues.miro la hora que nos ha pasado el admin y la formateamos para que funcione bien todo.
    Hacmos log de la inforamción del schedule.
    Finalmente realizamos unas comprobaciones y ejecutamos el DISPATCH JOB para realizar la entrega.
    Esto se ejecuta cada minuto

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -1,8 +1,25 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use App\Models\ConfigXuxes;
+use Illuminate\Support\Facades\Schedule;
+use Illuminate\Support\Facades\Log;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::call(function () {
+    $config = ConfigXuxes::first();
+    $horaConfig = $config->hora;
+    $horaActual = now()->format('H:i');
+
+    Log::info('Scheduler check', [
+        'hora_actual' => $horaActual,
+        'hora_config' => $horaConfig,
+        'ultima_entrega' => $config->ultima_entrega
+    ]);
+
+    if (
+        $horaActual >= $horaConfig &&
+        $config->ultima_entrega !== now()->toDateString()
+    ) {
+        Log::info('DISPATCH JOB');
+        \App\Jobs\RepartirXuxesDiarias::dispatchSync();
+    }
+})->everyMinute();
