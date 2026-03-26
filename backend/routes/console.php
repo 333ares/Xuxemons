a568e79e9ea76a99ea3b3a0e04a781b73ce86c82
333ares <aresgp5335@gmail.com>
2026-03-26 19:14:11 +0100

    Schedule creado

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\ConfigXuxes;
+use App\Models\ConfigXuxemon;
 use Illuminate\Support\Facades\Schedule;
 use Illuminate\Support\Facades\Log;
 
@@ -22,5 +23,26 @@ Schedule::call(function () {
     ) {
         Log::info('DISPATCH JOB');
         \App\Jobs\RepartirXuxesDiarias::dispatchSync();
+    }
+})->everyMinute();
+
+Schedule::call(function () {
+    $config = ConfigXuxemon::first();
+
+    $horaConfig = \Carbon\Carbon::createFromFormat('H:i', $config->hora);
+    $horaActual = now();
+
+    Log::info('Scheduler xuxemon check', [
+        'hora_actual'    => $horaActual->format('H:i'),
+        'hora_config'    => $config->hora,
+        'ultima_entrega' => $config->ultima_entrega
+    ]);
+
+    if (
+        $horaActual->gte($horaConfig) &&
+        $config->ultima_entrega !== now()->toDateString()
+    ) {
+        Log::info('DISPATCH JOB XUXEMON');
+        \App\Jobs\RepartirXuxemonDiario::dispatchSync();
     }
 })->everyMinute();
